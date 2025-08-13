b60f19e24a249b75304e8c375c736f346c7d4029
DocenkoN <trudno123@yandex.ru>
2025-08-13 17:10:54 +0300

    настроил экран по макету в фигме, написал код

---
+++ ImageFeed/ImagesListCell.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Николай Доценко on 10.08.2025.
+//
+
+import Foundation
---
+++ ImagesList/ImagesListViewController.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+    }
+    
+    extension ImagesListViewController: UITableViewDelegate {
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    }
+
+
+
