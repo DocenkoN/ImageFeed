23a0f314430257530e394ecea9c5a8a283c5ce9c
DocenkoN <trudno123@yandex.ru>
2025-08-22 12:08:52 +0300

    добавил тап бар

--- ImagesList/ProfileViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProfileViewController.swift
-//  ImageFeed
-//
-//  Created by Николай Доценко on 19.08.2025.
-//
-
-import Foundation
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -3,19 +3,11 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     @IBOutlet weak var userPick: UIImageView!
-    
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var logoutButton: UIButton!
-    
     @IBOutlet weak var loginLabel: UILabel!
-    
     @IBOutlet weak var descriptionLabel: UILabel!
-    
     
     @IBAction func didTapLogoutButton(_ sender: Any) {
     }
-    
-    
-    
 }
