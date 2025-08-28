5a9b299e44b8db0e224e747fae32a421b579d93c
DocenkoN <trudno123@yandex.ru>
2025-08-28 15:18:22 +0300

    добавил константы

---
+++ Constant/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accsesKey = "HGZqGD_jDTvxAilpbSL-Ht8_GaIuTvkvtaPxuJfZWPg"
+    static let secretKEY = "k6n2d6jIvk36vHi9yixAGuZp4JuYaH2vbhc3A7V-p18"
+    static let redirectURL = "<urn:ietf:wg:oauth:2.0:oob>"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
+}
